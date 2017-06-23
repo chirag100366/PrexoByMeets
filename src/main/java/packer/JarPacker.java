94ba3e041c8e527870f59daabed4bb8aea6291da
Rohit Patiyal <rohit.patiyal@flipkart.com>
2017-06-23 06:18:43 +0530

    Added java based evaluator

---
+++ src/main/java/packer/JarPacker.java
@@ -0,0 +1,20 @@
+package packer;
+
+/**
+ * Created by rohitpatiyal on 23/6/17.
+ */
+public class JarPacker {
+    JarPacker(){
+//        PackrConfig config = new PackrConfig();
+//        config.platform = PackrConfig.Platform.Windows32;
+//        config.jdk = "/User/badlogic/Downloads/openjdk-for-mac.zip";
+//        config.executable = "myapp";
+//        config.classpath = Arrays.asList("myjar.jar");
+//        config.mainClass = "com.my.app.MainClass";
+//        config.vmArgs = Arrays.asList("Xmx1G");
+//        config.minimizeJre = "soft";
+//        config.outDir = new java.io.File("out-mac");
+//
+//        new Packr().pack(config);
+    }
+}
